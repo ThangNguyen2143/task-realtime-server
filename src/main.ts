28b2a29c99fddeeac2e7942d275b8419f14d5a83
ThangNguyen2143 <125419960+ThangNguyen2143@users.noreply.github.com>
2026-08-07 00:08:39 +0700

    Fix production start script path and update TypeScript configuration for module interoperability

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import cookieParser from 'cookie-parser';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+import cookieParser = require('cookie-parser');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
